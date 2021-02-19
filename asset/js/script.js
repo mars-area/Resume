9cbd37e7bd164326502e03b4c0c19d918edd2988
mars-area <ilhammargatama@gmail.com>
2021-02-19 14:27:50 +0700

    refactoring code for dark and light mode

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,6 +1,16 @@
 // Dark Mode
 const lightMode = document.querySelector(':root');
 const lightModeButton = document.querySelector('.btn');
+function lightColor() {
+    lightMode.style.setProperty('--color-1', '#eeeeee');
+    lightMode.style.setProperty('--color-2', '#222831');
+    lightMode.style.setProperty('--color-4', '#f7f7f7');
+}
+function darkColor() {
+    lightMode.style.setProperty('--color-1', '#222831');
+    lightMode.style.setProperty('--color-2', '#eeeeee');
+    lightMode.style.setProperty('--color-4', '#393e46');
+}
 
 function clickCounter() {
     if (typeof(Storage) !== "undefined") {
@@ -10,20 +20,11 @@ function clickCounter() {
         sessionStorage.clickcount = 1;
         }
         if (sessionStorage.clickcount % 2 == 0) {
-            lightMode.style.setProperty('--color-1', '#eeeeee');
-            lightMode.style.setProperty('--color-2', '#222831');
-            lightMode.style.setProperty('--color-4', '#f7f7f7');
+            lightColor();
         } else {
-            lightMode.style.setProperty('--color-1', '#222831');
-            lightMode.style.setProperty('--color-2', '#eeeeee');
-            lightMode.style.setProperty('--color-4', '#393e46');
+            darkColor();
         }
     } else {
       document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
     }
 }
-
-// Animasi
-const circle1 = document.querySelector('.c1');
-const circle2 = document.querySelector('.c2');
-const circle3 = document.querySelector('.c3');
